2289fac893a98c191fb86a31cb5c2748da6fb8fa
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-06-04 18:29:17 -0300

    Blink problem solved

--- src/app/posts/postfeed/postfeed.component.ts
+++ src/app/posts/postfeed/postfeed.component.ts
@@ -1,45 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { Http } from '@angular/http';
-import { BlogService } from '../../services/blog.service'
-import { PostsService } from '../posts.service'
-import { ActivatedRoute, Router  } from '@angular/router';
-
+import { BlogService } from '../../services/blog.service';
+import { PostsService } from '../posts.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
-  selector: 'app-postfeed',  
+  selector: 'app-postfeed',
   templateUrl: './postfeed.component.html',
   styleUrls: ['./postfeed.component.css']
 })
 export class PostfeedComponent implements OnInit {
 
-  public posts = []
+  public posts = [];
   public category;
   public page;
-  public postavailable:boolean = false
+  public postavailable = false;
 
-  constructor(private router: Router , private service: BlogService, private postservice: PostsService,
-    private activated_route: ActivatedRoute) {                    }
+  constructor(
+    private router: Router, private service: BlogService, private postservice: PostsService,
+    private activated_route: ActivatedRoute, private meta: Meta,
+    @Inject(PLATFORM_ID) private platform: Object
+  ) { }
 
-  ngOnInit() {  
-   this.activated_route.params.subscribe(params => {
-      this.category = params['category']   
-       if(this.category){
+  ngOnInit() {
+    this.activated_route.params.subscribe(params => {
+      this.category = params['category'];
+      if (this.category) {
         this.service.getcategoryposts(this.category).subscribe(data => {
-          this.posts = data
-          this.sortposts()
-        //  console.log(this.posts);        
-       })
-     }      
-   })
+          this.posts = data;
+          this.sortposts();
+        });
+      }
+    });
 
-    
-   if(!this.category) {      
-      this.service.getposts().subscribe(data => {
-        this.posts = data
-        this.sortposts()        
-      //  console.log(this.posts)
-      })
-    }    
+
+    if (!this.category) {
+      if (isPlatformBrowser(this.platform) && this.meta.getTag('name="API_DATA"')) {
+        this.posts = JSON.parse(this.meta.getTag('name="API_DATA"').getAttribute('content'));
+        this.meta.removeTag('name="API_DATA"');
+      } else {
+        this.service.getposts().subscribe(data => {
+          if (isPlatformServer(this.platform)) {
+            this.meta.addTag({ name: 'API_DATA', content: JSON.stringify(data) });
+          }
+
+          this.posts = data;
+          this.sortposts();
+        });
+      }
+    }
   }
 
   sortposts() {
@@ -54,7 +65,8 @@ export class PostfeedComponent implements OnInit {
       } else {
         return 0;
       }
-    })
-    this.postavailable = true    
+    });
+
+    this.postavailable = true;
   }
 }
